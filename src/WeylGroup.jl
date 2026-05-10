ec9395746b865cd92a1164ccea432768b8a692cd
Pieter Belmans <pieterbelmans@gmail.com>
2026-05-10 14:23:28 +0200

    fix: if a root becomes zero we are in trouble
    Maybe we don't have to test for this?

--- src/WeylGroup.jl
+++ src/WeylGroup.jl
@@ -269,11 +269,7 @@ function _explain_rmul(x::WeylGroupElem, s::UInt8, refl::AbstractMatrix{UInt}, r
 
     # Apply reflection s_{word[k]} to root
     root = refl[Int(x.word[k]), Int(root)]
-
-    if iszero(root)
-      # root is no longer a minimal root, meaning we found the best insertion point
-      return true, insert_index, insert_letter
-    end
+    iszero(root) && error("Reduced-word insertion invariant violated in right multiplication")
 
     # Check if we have a better insertion point.
     # Since word[k] is a simple root, if root < word[k] it must also be simple.
